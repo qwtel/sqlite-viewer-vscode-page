0e28ac73d5b25f67b70e4f4ff189d23b8c4e2c8b
Florian Klampfer <mail@qwtel.com>
2024-10-15 18:12:53 +0700

    Commit

--- functions/api/register.ts
+++ functions/api/register.ts
@@ -118,3 +118,24 @@ type ResponseData = {
   success: false;
   message: string;
 };
+
+// Set CORS to all /api responses
+export const onRequest: PagesFunction = async (context) => {
+  const response = await context.next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response;
+};
+
+// Respond to OPTIONS method
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
+};
