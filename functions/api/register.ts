45c8b9efb4cd9ade808251fee61e964be04f07f2
Florian Klampfer <mail@qwtel.com>
2024-10-22 22:03:42 +0700

    Commit

--- functions/api/register.ts
+++ functions/api/register.ts
@@ -3,11 +3,13 @@
 import * as jose from 'jose'
 import { badRequest, internalServerError, paymentRequired, serviceUnavailable, unsupportedMediaType } from '@worker-tools/response-creators'
 import { corsMiddleware, corsOptions, Env, ResponseData } from './#shared';
+import { html, HTMLResponse } from '@worker-tools/html';
+import { contentTypes, withMiddleware as applyMiddleware } from "@worker-tools/middleware"
 
 // Respond to OPTIONS method
 export const onRequestOptions = corsOptions;
 
-export const onRequestPost: PagesFunction<Env>[] = [corsMiddleware, async (context) => {
+export const onRequestPost: PagesFunction<Env>[] = [corsMiddleware, async (context) => applyMiddleware(contentTypes(['text/html', 'application/json', '*/*']), async (_req, { type }) => {
   try {
     if (context.request.headers.get('Content-Type') !== 'application/x-www-form-urlencoded') return unsupportedMediaType(); 
     const fd = new URLSearchParams(await context.request.text());
@@ -56,15 +58,39 @@ export const onRequestPost: PagesFunction<Env>[] = [corsMiddleware, async (conte
     if (purchase.disputed || purchase.chargebacked || purchase.refunded)
       return paymentRequired('Purchase was disputed, chargebacked or refunded');
 
+    const includeKey = fd.get('include_key') != null;
+
     const jwtKey = await jose.importPKCS8(context.env.JWT_PRIVATE_KEY_PKCS8, 'ES256');
-    const token = await new jose.SignJWT({ pid: purchase.id })
+    const token = await new jose.SignJWT({ pid: purchase.id, ...includeKey ? { licenseKey } : {} })
       .setProtectedHeader({ alg: 'ES256' })
       .setIssuedAt()
-      .setExpirationTime('15d')
-      .sign(jwtKey)
-    
-    return Response.json({ token }, { headers: [['Authorization', `Bearer ${token}`]] });
+      .setExpirationTime(includeKey ? '12weeks' : '15d')
+      .sign(jwtKey);
+
+    if (type === 'text/html') {
+      return new HTMLResponse(html`<html>
+        <body>
+          <h1>Access Token Generated</h1>
+          <textarea readonly rows="8" cols="60">${token}</textarea>
+        </body>
+      </html>`);
+    } else {
+      return Response.json({ token }, { headers: [['Authorization', `Bearer ${token}`]] });
+    }
   } catch (err) {
     return internalServerError(`Unexpected service error: ${err instanceof Error ? err.message: err}`);
   }
+})(context.request)];
+
+export const onRequestGet: PagesFunction<Env>[] = [corsMiddleware, async (context) => {
+  return new HTMLResponse(html`<html>
+    <body>
+      <h1>License Key to Access Token</h1>
+      <form method="post">
+        <input type="text" name="license_key" placeholder="Enter license key">
+        <input type="hidden" name="include_key" value="">
+        <input type="submit" value="Generate">
+      </form>
+    </body>
+  </html>`)
 }];
