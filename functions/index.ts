3d3541547fef3590c1196a98c1252d71a763c3ac
Florian Klampfer <mail@qwtel.com>
2024-11-27 11:16:37 +0700

    Commit

--- functions/index.ts
+++ functions/index.ts
@@ -14,13 +14,13 @@ const V2Countries = new Set([
 
 const PROEditionHrefs = {
   live: 'https://buy.polar.sh/polar_cl_zWrh5cQnCfn0WQtRjoKdN4hL_5uIMaFuKh8kazgiOaA',
-  sandbox: 'https://sandbox-api.polar.sh/v1/checkout-links/polar_cl_6UZUT7AMFnMuxYbwQFGA4shpYnByJYNrbUGlysKk5IM/redirect',
+  sandbox: 'https://sandbox-api.polar.sh/v1/checkout-links/polar_cl_oZZ0E-0oiRaoD6Uv4RaZbGS5USgOFzOSzAYjNcCv88A/redirect',
   legacy: 'https://qwtel.gumroad.com/l/smzwr',
 };
 
 const BusinessEditionHrefs = {
   live: 'https://buy.polar.sh/polar_cl_1nt1xmGrmwZCRfex_41LcNHEnrGI3IRpry7ofTaJACQ',
-  sandbox: 'https://sandbox-api.polar.sh/v1/checkout-links/polar_cl_ydGFTF7KnKS0U5UP23Yi2GhRxUlr-OqSBNBvng40kYE/redirect',
+  sandbox: 'https://sandbox-api.polar.sh/v1/checkout-links/polar_cl__kwPKYyg4LrSAeWV1zpZvnpoY4Aqlh0toxs6W0tjTII/redirect',
   legacy: 'https://qwtel.gumroad.com/l/smzwr?option=lFAu5YJXnIoi7WmG79HCsQ%3D%3D',
 };
 
