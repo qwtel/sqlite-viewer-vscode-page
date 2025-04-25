08663406bc7271a86effdb858eeba4a529d93e9c
Florian Klampfer <mail@qwtel.com>
2025-04-25 15:45:02 +0200

    Commit

--- src/js/main.js
+++ src/js/main.js
@@ -152,5 +152,81 @@
       outObserver.observe(div);
     });
   }
+
+  const sheet = new CSSStyleSheet();
+  sheet.replaceSync(`
+.lds-ring {
+  color: functions.color(typography, 2);
+}
+.lds-ring,
+.lds-ring div {
+  box-sizing: border-box;
+}
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid currentColor;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: currentColor transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+  `);
+  document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+
+  const isNewTab = ev => ev.ctrlKey || ev.metaKey || ev.shiftKey || ev.button === 1;
+  const showSpinner = () => {
+    const muOb = new MutationObserver((muts) => {
+      for (const mut of muts) {
+        for (const node of mut.addedNodes) {
+          if (node instanceof HTMLElement) {
+            const child = node.children[0];
+            if (child && child.classList.contains('polar-loader-spinner')) {
+              node.innerHTML = '';
+              node.style.position = 'fixed';
+              node.style.top = node.style.left = '0px';
+              node.style.width = node.style.height ='100%';
+              node.style.transform = ''
+              node.style.display = 'grid';
+              node.style.placeItems = 'center';
+              node.insertAdjacentHTML('beforeend', '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>');
+              muOb.disconnect();
+            }
+          }
+        }
+      }
+    })
+    muOb.observe(document.body, { childList: true });
+  };
+
+  document.querySelectorAll('[data-polar-checkout]').forEach(el => {
+    el.addEventListener('click', ev => isNewTab(ev) ? ev.stopImmediatePropagation() : showSpinner());
+  });
 })()
 
