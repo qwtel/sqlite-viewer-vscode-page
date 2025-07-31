e967bec79315a0a9a057c6d5a7334d3f66ae5fbe
Florian Klampfer <mail@qwtel.com>
2025-07-31 09:26:34 +0700

    Commit

--- functions/index.ts
+++ functions/index.ts
@@ -104,7 +104,7 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
 
   if (hasDiscount) {
     rewriter = rewriter
-      .on('.pricing-toggle-container, .monthly-price', {
+      .on('.i18n-hide, .pricing-toggle-container, .monthly-price', {
         element(el) {
           el.remove();
         }
